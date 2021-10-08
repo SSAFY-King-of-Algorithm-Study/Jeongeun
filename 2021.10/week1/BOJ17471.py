378fb3f0f3b29494ea8dd9a235abee84e0f3dc9b
Sojjeong <tip9004@gmail.com>
2021-10-08 23:42:48 +0900

    게리맨더링 해결

--- 2021.10/week1/BOJ17471.py
+++ 2021.10/week1/BOJ17471.py
@@ -1,116 +1,71 @@
 # 게리맨더링
+from collections import deque
+import itertools
+import sys
 
 
+def bfs(combi):
+    q = deque([combi[0]])
+    sum = 0
+    visited = set()
+    flag = False
 
-# 서로 연결되어 있는지를 확인할 함수 하나
-def isConnected(i, A_list, cnt, isVisited, N):
-    if cnt == N:
-        return
+    while q:
+        node = q.popleft()
 
-    temp_list = arr[i]
+        # 방문하지 않은 노드이면
+        if node not in visited:
+            sum += population[node]
+            visited.add(node)
 
-    for j in range(len(temp_list)):
-        if temp_list[j] == 1 and j in A_list and not isVisited[j]:
-            isVisited[j] = True
-            isConnected(j, A_list, cnt+1, isVisited, N)
+            # 그 노드에 연결된 다른 정점 확인
+            for u in board[node]:
+                # 조합 안에 있는지, 이미 방문한 노드가 아닌지 확인
+                if u in combi and u not in visited:
+                    q.append(u)
 
-
-def combination(index, cnt, N):
-    if cnt == N : return
-    if index == N: return
-    
-    global minResult
-
-    if cnt > 0:
-        # 적어도 한개의 영역을 포함해야 하니까
-        A_list = []
-        B_list = []
-
-        for i in range (N):
-            if isSelected[i]:
-                A_list.append(i)
-            else:
-                B_list.append(i)
-        
-        sumA = 0
-        sumB = 0
-
-        isVisitedA = [False for _ in range (N)]
-        isVisitedB = [False for _ in range (N)]
-
-        cntA = 0
-        cntB = 0
-
-        for i in A_list:
-            isConnected(i, A_list, 0, isVisitedA, len(A_list))
-
-        for i in A_list:
-            if isVisitedA[i]:
-                cntA += 1
-
-        if cntA == len(A_list):
-            for idx in A_list:
-                sumA += population[idx]
-        else:
-            return
-        
-        for i in B_list:
-            isConnected(i, B_list, 0, isVisitedB, len(B_list))
-
-        for i in B_list:
-            if isVisitedB[i]:
-                cntB += 1
-        
-        if cntB == len(B_list):
-            for idx in B_list:
-                sumB += population[idx]
-        else:
-            return
-
-        minResult = min(minResult, abs(sumA - sumB))
-
-    isSelected[index] = True
-    combination(index+1, cnt+1, N)
-
-    isSelected[index] = False
-    combination(index+1, cnt, N)
-
+    # 조합 수 만큼 while문이 돌았다는 건, 다 방문이 가능한 점이라는 것
+    if len(combi) == len(visited): flag = True
+    return sum, flag
 
 
 N = int(input())
-
 population = list(map(int, input().split()))
+board = [[] for _ in range(N)]
 
-arr = [[-1 for _ in range(N)] for _ in range(N)]
-
-for i in range(N):
-    for j in range(N):
-        if i == j : arr[i][j] = 0
-        else : arr[i][j] = -1
-
+# 인접 행렬 말고, 인접 리스트로 표현해보기
 for i in range(N):
     input_list = list(map(int, input().split()))
 
     for j in range(1, len(input_list)):
-        # 양뱡향 그래프
-        arr[i][input_list[j]-1] = 1
-        arr[input_list[j]-1][i] = 1
+        board[i].append(input_list[j]-1) # 0부터 시작하게..
 
-# 두개로 선거구를 나누는 방법
-
-isSelected = [False for _ in range (N)]
-location = [i for i in range (N)]
 
 # 될 수 있는 최고 수 + 1
 global minResult
-minResult = 1001
+minResult = sys.maxsize
 
-combination(0, 0, N)
+# 두개로 선거구를 나누는 방법 -> 조합으로 두개로 나눈 후, bfs 이용해 각 조합이 연결되어 있는지 확인
+# itertools 이용해 조합 구하기 
+# 조합이 1 2 3 이라고 생각 했을 때, (1 / 2 3) 과 (2 3 / 1) 이 같으므로 반절만 구함!
+for i in range(1, N // 2 + 1 ):
+    # range(N) 까지의 수를 i개로 나눔
+    combis = list(itertools.combinations(range(N), i))
 
+    for combi in combis:
+        # 두 선거구가 연결되어 있는지 확인
+        # 조합에서 선택한 값
+        sum1, flag1 = bfs(combi)
+        
+        # 조합에서 선택하지 않은 나머지 값
+        sum2, flag2 = bfs([i for i in range(N) if i not in combi])
 
-print(minResult)
-
-
-
-
-
+        # 둘 다 연결이 되어 있다면 true 반환
+        if flag1 and flag2:
+            if abs(sum1 - sum2) < minResult:
+                minResult = abs(sum1 - sum2)
+        
+if minResult == sys.maxsize:
+    print(-1)
+else:
+    print(minResult)
